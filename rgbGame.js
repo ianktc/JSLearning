7afd52d908b0488b816f0cd4c8f69c5afd7a16ff
ianchan <ianktc14@gmail.com>
2020-05-08 20:27:18 -0400

    colour game done

--- rgbGame.js
+++ rgbGame.js
@@ -1,18 +1,23 @@
-var colours = createArray();
+var size = 6;
+var colours = createArray(size);
 var pickedColour; 
 var squares = document.querySelectorAll(".square");
 
-//change title to picked rgb
-var colourDisplay = document.getElementById("colourDisplay");
-var message = document.querySelector("#message");
+//start with hard
+document.querySelector("#easy").classList.remove("selected");
+document.querySelector("#hard").classList.add("selected");
 
 //play game
 game();
 
-//game function
+//main game function
 function game(){
 
-	document.querySelector("h1").style.backgroundColor = "#232323";
+	//change title to picked rgb
+	var colourDisplay = document.getElementById("colourDisplay");
+	var message = document.querySelector("#message");
+
+	document.querySelector("h1").style.backgroundColor = "steelblue";
 	document.querySelector("#reset").textContent = "New Colours";
 
 	colourDisplay.textContent = pickedColour;
@@ -27,7 +32,7 @@ function game(){
 
 			//detect if correct or incorrect
 			if(this.style.backgroundColor === pickedColour){
-				win();
+				win(size);
 			} else {
 				message.textContent = "Try Again";
 				this.style.backgroundColor = "#232323";
@@ -36,11 +41,37 @@ function game(){
 		});
 	}
 
+	//condition for easy mode
+	if(size == 3){
+		for(i = 3; i < 6; i++){
+			squares[i].style.backgroundColor = "#232323";
+		}
+	}
+
 }
+
+//easy and hard buttons
+document.querySelector("#easy").addEventListener("click", function(){
+	document.querySelector("#easy").classList.add("selected");
+	document.querySelector("#hard").classList.remove("selected");
+	size = 3;
+	colours = createArray(size);
+	game();
+});
+
+document.querySelector("#hard").addEventListener("click", function(){
+	document.querySelector("#easy").classList.remove("selected");
+	document.querySelector("#hard").classList.add("selected");
+	size = 6;
+	colours = createArray(size);
+	game();
+});
 
 //reset button event
 document.querySelector("#reset").addEventListener("click", function(){
-	colours = createArray();
+	colours = createArray(size);
+	message.textContent = "";
+	document.querySelector("h1").style.backgroundColor = "steelblue";
 	game();
 });
 
@@ -56,25 +87,28 @@ function colourConstruct(){
 }
 
 //populate array
-function createArray(){
+function createArray(size){
 	var colours = [];
 
-	for(i = 0; i < 6; i++){
+	for(i = 0; i < size; i++){
 		colour = colourConstruct();
 		colours.push(colour);
 	}
 
-	pickedColour = colours[Math.floor(Math.random() * 6)];
+	if(size == 6) pickedColour = colours[Math.floor(Math.random() * 6)];
+	else pickedColour = colours[Math.floor(Math.random() * 3)];
 	
 	return colours;
 }
 
 //win - set all squares to same colour
-function win(){
+function win(size){
 	message.textContent = "Correct";
-	for(i = 0; i < squares.length; i++){
+
+	for(i = 0; i < size; i++){
 		squares[i].style.backgroundColor = pickedColour;
 	}
+
 	document.querySelector("h1").style.backgroundColor = pickedColour;
 	document.querySelector("#reset").textContent = "Play Again?";
 }
