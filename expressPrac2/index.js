ecaf66810eba03d0c2c7975b729a5c064d181e28
ianchan <ianktc14@gmail.com>
2020-05-14 23:43:14 -0400

    more express application practice

--- expressPrac2/index.js
+++ expressPrac2/index.js
@@ -3,12 +3,23 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
+//use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
 //serve contents of public directory (including style sheets)
 app.use(express.static("public"));
 
 //forgo file extensions (tell it to expect ejs files)
 app.set("view engine", "ejs");
 
+
+
+var friends = [ "John", "Amy", "Ana", "Alex", "Susan"];
+
+
+//home directory
 app.get("/", function(req, res){
 	//can render html files - like dynamic html files
 	res.render("home.ejs");
@@ -16,6 +27,7 @@ app.get("/", function(req, res){
 	//res.send("<h1>Welcome</h1>");
 });
 
+//love page
 app.get("/fallinlovewith/:thing", function(req, res){
 	var thing = req.params.thing;
 
@@ -23,14 +35,17 @@ app.get("/fallinlovewith/:thing", function(req, res){
 	res.render("love.ejs", {thingVar: thing});
 });
 
-app.get("/posts", function(req, res){
-	var posts = [
-		{title: "Post 1", author: "Person"},
-		{title: "Post 2", author: "Person"},
-		{title: "Post 3", author: "Person"}
-	];
+app.post("/addFriend", function(req, res){
+	var newFriend = req.body.newFriend;
+	console.log(req.body.newFriend);
+	console.log(req.body);
+	friends.push(newFriend);
+	res.redirect("/posts");
+});
 
-	res.render("posts.ejs", {posts: posts});
+//posts page
+app.get("/posts", function(req, res){
+	res.render("posts.ejs", {friends: friends});
 });
 
 app.listen(3000, function(){
